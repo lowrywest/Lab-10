6c93995ff7a76df2b6f840337511c0ef680b00db
Lowry West <4lowrywest@gmail.com>
2019-04-11 10:56:56 -0500

    Shape class work

--- Shape.java
+++ Shape.java
@@ -1,4 +1,30 @@
+import java.awt.Color;
+import java.awt.Point;
 
-public class Shape {
-
+public class Shape 
+{
+	private Point[] location;
+	private Color color;
+	private boolean filled;
+	
+	public Shape(Color color, boolean filled)
+	{
+		this.color=color;
+		this.filled=filled;
+	}
+	
+	public Color getColor()
+	{
+		return color;
+	}
+	
+	public boolean isFilled()
+	{
+		return filled;
+	}
+	
+	public Point[] getLocation()
+	{
+		return location;
+	}
 }
