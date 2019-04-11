c22124a1f2c0a1b99d3619edee2cc2b164916edb
Lowry West <4lowrywest@gmail.com>
2019-04-11 13:48:12 -0500

    Adding files and work

--- Rectangle.java
+++ Rectangle.java
@@ -1,4 +1,18 @@
+import java.awt.Color;
+import java.awt.Point;
 
-public class Rectangle {
+/*
+ * @version April 11, 2019 Lab 10
+ * @author Lowry West
+ */
+
+public class Rectangle extends Polygon
+{
+
+	public Rectangle(Point pointUL, int width,int height,Color color,boolean filled)
+	{
+		super(color, filled);
+		// TODO Auto-generated constructor stub
+	}
 
 }
--- RightTriangle.java
+++ RightTriangle.java
@@ -1,4 +1,24 @@
+import java.awt.Color;
+import java.awt.Point;
 
-public class RightTriangle {
+/*
+ * @author Stephen
+ * @author Lowry West
+ * @version April 11, 2019
+ */
+
+
+public class RightTriangle extends Triangle
+{
+	private int base;
+	private int height;
+
+	public RightTriangle(Point pointLB, int base, int height,Color color, boolean filled) 
+	{
+		super(pointLB, color, filled);
+		this.base=base;
+		this.height=height;
+		// TODO Auto-generated constructor stub
+	}
 
 }
